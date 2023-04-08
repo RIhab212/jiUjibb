e3419153e29a324ad7cec060621c687417aef07d
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-04-08 13:40:50 +0200

    wrbsite

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
 		  } else if (status === "user logged in") {
 			alert("login successful");
 			window.localStorage.setItem("token", data.data);
-			window.location.href = "./jiUjibb/FormC";
+			window.location.href = "./FormC";
 		  } else {
 			setError("Invalid login status");
 		  }
