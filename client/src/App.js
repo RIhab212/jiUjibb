5cbd9caac7451d063f3f433af55f81012ef61b87
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-04-08 02:16:13 +0200

    wrbsite

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,15 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-<<<<<<< HEAD
-      <Routes>
-          <Route path="/Login" exact element={<Login />} />
-
-=======
+<BrowserRouter>
       <Routes> 
         <Route path="/Login" exact element={<Login />} />
->>>>>>> 96b5623340df62c2ef751a8d8c6806766598cd86
         <Route path="/signup" element={< Signup/>} />
         <Route path="/adminespace" element={<AdminInterface/>}/>
         <Route path="/userLoggedInDetails" element={<UserLoggedInDetails/>}/>
          <Route path="/validrecords" element={<Validrecords/>}/>
-         <Route path="/Products"exact element={<FormC/>}/>
-         <Route path="/Phonesignup"exact element={<Phonesignup/>}/>
+         <Route path="/Products" element={<FormC/>}/>
+         <Route path="/Phonesignup" element={<Phonesignup/>}/>
          <Route path="/validrecords" element={<Validrecords/>}/>
          <Route path="/recordList" element={<RecordList/>}/>
 
